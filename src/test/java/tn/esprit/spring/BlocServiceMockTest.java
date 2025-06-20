0d88b9b6e301eadd26213f6e25993e24e35cc273
jihedbouabid <jihedbouabid@gmail.com>
2025-06-20 19:41:53 +0100

    feat: adding jacoco

--- src/test/java/tn/esprit/spring/BlocServiceMockTest.java
+++ src/test/java/tn/esprit/spring/BlocServiceMockTest.java
@@ -92,4 +92,70 @@ public class BlocServiceMockTest {
         assertEquals(2, result.size());
         verify(blocRepository).findAll();
     }
+    @Order(5)
+    @Test
+    void test_delete_blocWithChambres() {
+        Bloc bloc = new Bloc();
+        Chambre ch1 = new Chambre(); Chambre ch2 = new Chambre();
+        bloc.setChambres(List.of(ch1, ch2));
+
+        blocService.delete(bloc);
+
+        verify(chambreRepository).deleteAll(bloc.getChambres());
+        verify(blocRepository).delete(bloc);
+    }
+    @Order(6)
+    @Test
+    void test_affecterChambresABloc() {
+        String blocName = "BlocTest";
+        Bloc bloc = new Bloc();
+        bloc.setNomBloc(blocName);
+
+        Chambre ch1 = new Chambre(); ch1.setNumeroChambre(101L);
+        Chambre ch2 = new Chambre(); ch2.setNumeroChambre(102L);
+
+        when(blocRepository.findByNomBloc(blocName)).thenReturn(bloc);
+        when(chambreRepository.findByNumeroChambre(101L)).thenReturn(ch1);
+        when(chambreRepository.findByNumeroChambre(102L)).thenReturn(ch2);
+
+        Bloc result = blocService.affecterChambresABloc(List.of(101L, 102L), blocName);
+
+        assertEquals(bloc, result);
+        assertEquals(bloc, ch1.getBloc());
+        assertEquals(bloc, ch2.getBloc());
+        verify(chambreRepository, times(2)).save(any(Chambre.class));
+    }
+
+    @Order(7)
+    @Test
+    void test_ajouterBlocEtSesChambres() {
+        Bloc bloc = new Bloc();
+        Chambre ch1 = new Chambre(); Chambre ch2 = new Chambre();
+        bloc.setChambres(List.of(ch1, ch2));
+
+        Bloc result = blocService.ajouterBlocEtSesChambres(bloc);
+
+        assertEquals(bloc, ch1.getBloc());
+        assertEquals(bloc, ch2.getBloc());
+        verify(chambreRepository, times(2)).save(any(Chambre.class));
+        assertEquals(bloc, result);
+    }
+
+    @Order(8)
+    @Test
+    void test_ajouterBlocEtAffecterAFoyer() {
+        Bloc bloc = new Bloc();
+        String nomFoyer = "Foyer Test";
+        Foyer foyer = new Foyer();
+        foyer.setNomFoyer(nomFoyer);
+
+        when(foyerRepository.findByNomFoyer(nomFoyer)).thenReturn(foyer);
+        when(blocRepository.save(bloc)).thenReturn(bloc);
+
+        Bloc result = blocService.ajouterBlocEtAffecterAFoyer(bloc, nomFoyer);
+
+        assertEquals(foyer, bloc.getFoyer());
+        verify(blocRepository).save(bloc);
+        assertEquals(bloc, result);
+    }
 }
