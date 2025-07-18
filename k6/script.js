8c094a7806ef01f68f66dffcfdaf48549118b5a8
jihedbouabid <jihedbouabid@gmail.com>
2025-07-18 07:15:44 +0100

    feat: load test update

--- k6/script.js
+++ k6/script.js
@@ -2,18 +2,17 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 10, // Number of virtual users
-    duration: '30s', // Duration of the test
+    vus: 10,
+    duration: '30s',
 };
 
 export default function () {
-    // Replace with your actual endpoint path
-    const res = http.get('http://localhost:8086/Foyer/etudiant');
+    const res = http.get('http://host.docker.internal:8086/Foyer/etudiant/findAll');
 
     check(res, {
         'status is 200': (r) => r.status === 200,
         'response time < 500ms': (r) => r.timings.duration < 500,
     });
 
-    sleep(1); // Pause for 1 second between requests
+    sleep(1);
 }
